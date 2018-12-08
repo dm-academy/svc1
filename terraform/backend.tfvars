0b4c2328f8b21e78c42e62963ccd607bbcb2c5bf
Donovan Smith <smit7209@stthomas.edu>
2018-12-07 20:12:53 -0600

    fixing init

---
+++ terraform/backend.tfvars
@@ -0,0 +1,3 @@
+storage_account_name="terraformseis664
+container_name="terraform"
+key="demo-catgif.terraform.tfstate"
--- terraform/init.sh
+++ terraform/init.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-ls
-pwd
-terraform init 
-#   -backend-config="storage_account_name=terraformseis664" \
-#   -backend-config="container_name=terraform" \
-#   -backend-config="key=demo-catgif.terraform.tfstate"
+terraform init -backend-config=backend.tfvars
