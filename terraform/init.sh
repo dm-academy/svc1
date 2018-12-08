2eb750f8efa727ec6a78487ab393848bf3a546bb
donovansmith <43284540+donovansmith@users.noreply.github.com>
2018-12-07 18:27:36 -0600

    Update init.sh

--- terraform/init.sh
+++ terraform/init.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-terraform init
+terraform init \
+   -backend-config="storage_account_name=terraformseis664" \
+   -backend-config="container_name=terraform" \
+   -backend-config="key=demo-catgif.terraform.tfstate"
